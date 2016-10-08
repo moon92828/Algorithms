30d91bd422604c03d89805cf50578c39771336c2
moon92828 <moon92828@163.com>
2016-10-09 00:08:21 +0800

    merge

---
+++ src/com/nightmoon/Merge.java
@@ -0,0 +1,42 @@
+package com.nightmoon;
+
+public class Merge {
+	private static Comparable[] aux;
+	
+	public static void merge(Comparable[] a,int lo,int mid,int hi){
+		int i=lo,j=mid+1;
+		for(int k=lo;k<=hi;k++){
+			aux[k]=a[k];
+		}
+		
+		for(int k=lo;k<=hi;k++){
+			if(i>mid) a[k]=aux[j++];
+			else if(j>hi) a[k]=aux[i++];
+			else if(less(aux[j],aux[i]))a[k]=aux[j++];
+			else a[k]=aux[i++];
+		}
+	}
+	
+	public static void sort(Comparable[] a){
+		aux=new Comparable[a.length];
+		sort(a,0,a.length-1);
+	}
+	
+	private static void sort(Comparable[] a,int lo,int hi){
+		if(hi<=lo)return;
+		int mid=lo+(hi-lo)/2;
+		sort(a,lo,mid);
+		sort(a,mid+1,hi);
+		merge(a,lo,mid,hi);
+	}
+	
+	private static boolean less(Comparable a,Comparable b){
+		return a.compareTo(b)<0;
+	}
+	
+	private static void exch(Comparable[] a,int i,int j){
+		Comparable temp=a[i];
+		a[i]=a[j];
+		a[j]=temp;
+	}
+}
